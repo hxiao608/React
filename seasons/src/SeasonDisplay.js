7b50e63ab0781824e4b3e06582b19dda4fc7f27e
Haoru <Harold@eduroam-169-233-212-160.ucsc.edu>
2019-10-31 13:26:08 -0700

    refactor ternary expression by using configuration file

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+//configuration file
+const seasonConfig = {
+  sumemr: {
+    text: "Lets hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, it is chilly",
+    iconName: "snowflake"
+  }
+};
+
 //function to determine what the season is (summer/winter)
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -13,12 +25,14 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
   //props.lat passed by components as props
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = //determine the text displayed on the screen
-    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+
+  const { text, iconName } = seasonConfig[season]; // return {text, iconName}
 
   return (
     <div>
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
