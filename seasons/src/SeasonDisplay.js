f384870434a26d4043df4e2ae106baf7596e8072
Haoru <Harold@eduroam-169-233-212-160.ucsc.edu>
2019-10-31 13:13:32 -0700

    determine show text using ternary expression

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,10 +1,26 @@
 import React from "react";
 
+//function to determine what the season is (summer/winter)
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    //northern summer and southern winter
+    return lat > 0 ? "summer" : "winter"; //ternary expression (true: norther, false: southern)
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
 const SeasonDisplay = props => {
   //props.lat passed by components as props
-  console.log(props);
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text = //determine the text displayed on the screen
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
 
-  return <div>Season Display</div>;
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
