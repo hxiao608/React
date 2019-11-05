e3b799969b09465a79a87c62dd14131d643927ce
Haoru <haoruxiao@gmail.com>
2019-11-05 11:27:12 -0800

    resolve undefined 'this' error

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -9,19 +9,24 @@ class SearchBar extends React.Component {
     console.log(event.target.value); //get value from user
   }
 
+  //prevent undefined 'this' error
+  onFormSubmit = event => {
+    event.preventDefault(); //prevent from submit the form and refresh the page
+    console.log(this.state.term); //undefined error8
+  };
+
   render() {
     //controlled component
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
               onChange={e => {
-                console.log(e.target.value);
-                this.setState({ term: e.target.value.toUpperCase() });
+                this.setState({ term: e.target.value });
               }} //alternative event handler
             />
           </div>
